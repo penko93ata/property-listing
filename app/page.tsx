45886c6902b347b144444e41b205c2eaef106b09
Penko Atanasov <atanasovpenko@gmail.com>
2024-03-23 16:55:19 +0200

    Adding hero component with shadcn select and input

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
+import { Hero } from "@/components/Hero";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <div>
-        <h1 className='text-3xl'>Welcome</h1>
-        <Link href='/properties'>Show Properties</Link>
-      </div>
-    </div>
+    <>
+      <Hero />
+    </>
   );
 }
