59683ed5189da05547d96d50c5520f9e35787b68
Penko Atanasov <atanasovpenko@gmail.com>
2024-04-30 13:57:08 +0300

    Creating zod schema and inferring types

--- types/properties.types.ts
+++ types/properties.types.ts
@@ -1,3 +1,5 @@
+import { z } from "zod";
+
 export type Property = {
   id: string;
   owner: string;
@@ -31,3 +33,45 @@ export type Rates = {
   monthly?: number;
   nightly?: number;
 };
+
+const PropertyAddFormSchema = z.object({
+  name: z.string().min(2, { message: getErrorMessages().min(2) }),
+  type: z.string().min(2, { message: getErrorMessages().min(2) }),
+  description: z.string().min(2, { message: getErrorMessages().min(10) }),
+  location: z.object({
+    street: z.string().min(2, { message: getErrorMessages().min(2) }),
+    city: z.string().min(2, { message: getErrorMessages().min(2) }),
+    state: z.string().min(2, { message: getErrorMessages().min(2) }),
+    zipcode: z.string().min(2, { message: getErrorMessages().min(2) }),
+  }),
+  beds: z.number(),
+  baths: z.number(),
+  square_feet: z.number(),
+  amenities: z.array(z.string()).optional(),
+  rates: z.object({
+    weekly: z.number().optional(),
+    monthly: z.number().optional(),
+    nightly: z.number().optional(),
+  }),
+  seller_info: z.object({
+    name: z.string().min(2, { message: getErrorMessages().min(2) }),
+    phone: z.string().min(2, { message: getErrorMessages().min(2) }),
+    email: z.string().email(),
+  }),
+  images: z.array(z.string()).optional(),
+  isFeatured: z.boolean().optional(),
+});
+
+export type TPropertyAddFormValues = z.infer<typeof PropertyAddFormSchema>;
+export interface IProperty extends TPropertyAddFormValues {
+  id: string;
+  owner: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+function getErrorMessages() {
+  return {
+    min: (minLength: number) => `Field must be at least ${minLength} characters long`,
+  };
+}
