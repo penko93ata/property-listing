7da41899c3df25ea3012eb2278e4072bd098207a
Penko Atanasov <penkoatanasov@Penkos-Laptop.local>
2024-08-10 15:10:42 +0300

    Making error page a client component

--- app/error.tsx
+++ app/error.tsx
@@ -1,5 +1,6 @@
+"use client";
 import Link from "next/link";
-import { FaExclamationTriangle } from "react-icons/fa";
+import { FaExclamationCircle } from "react-icons/fa";
 
 interface ErrorPageProps {
   error: Error;
@@ -11,7 +12,7 @@ export default function ErrorPage({ error }: ErrorPageProps) {
       <div className='container m-auto max-w-2xl py-24'>
         <div className='bg-white px-6 py-24 mb-4 shadow-md rounded-md border m-4 md:m-0'>
           <div className='flex justify-center'>
-            <FaExclamationTriangle className='text-8xl text-yellow-400' />
+            <FaExclamationCircle className='text-8xl text-yellow-400' />
           </div>
           <div className='text-center'>
             <h1 className='text-3xl font-bold mt-4 mb-2'>Something Went Wrong</h1>
