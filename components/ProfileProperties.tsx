afda2bb77d1b26af3f398f2a2d44435271daf927
Penko Atanasov <atanasovpenko@gmail.com>
2024-08-14 17:38:15 +0300

    Showing user properties

---
+++ components/ProfileProperties.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { TProperty } from "@/types/properties.types";
+import Image from "next/image";
+import { useState } from "react";
+
+export default function ProfileProperties({ properties: initialProperties }: { properties: TProperty[] }) {
+  const [properties, setProperties] = useState(initialProperties);
+  return properties.map((property) => (
+    <div key={property.id} className='mb-10'>
+      <a href='/property.html'>
+        <Image className='h-32 w-full rounded-md object-cover' src={property.images[0]} alt='' width={1000} height={200} />
+      </a>
+      <div className='mt-2'>
+        <p className='text-lg font-semibold'>{property.name}</p>
+        <p className='text-gray-600'>
+          Address: {property.location.street} {property.location.city} {property.location.state}
+        </p>
+      </div>
+      <div className='mt-2'>
+        <a href='/add-property.html' className='bg-blue-500 text-white px-3 py-3 rounded-md mr-2 hover:bg-blue-600'>
+          Edit
+        </a>
+        <button className='bg-red-500 text-white px-3 py-2 rounded-md hover:bg-red-600' type='button'>
+          Delete
+        </button>
+      </div>
+    </div>
+  ));
+}
