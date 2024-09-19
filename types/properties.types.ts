a346848156845d458cd42c9340c1614964d6ab92
Penko Atanasov <atanasovpenko@gmail.com>
2024-09-19 14:50:59 +0300

    Removing unused schemas

--- types/properties.types.ts
+++ types/properties.types.ts
@@ -1,11 +1,10 @@
 import { getErrorMessages } from "@/utils/getErrroMessages";
-import { optional, z } from "zod";
+import { z } from "zod";
 
 const { required, min } = getErrorMessages();
 
 const MAX_FILE_SIZE = 1024 * 1024 * 5; // 5MB
 const ACCEPTED_IMAGE_TYPES = ["image/jpeg", "image/jpg", "image/png", "image/webp"];
-// const ACCEPTED_IMAGE_TYPES = ["jpeg", "jpg", "png", "webp"];
 
 const getOptionalNumberSchema = () =>
   z
@@ -79,12 +78,6 @@ export const PropertyAddFormSchema = z.object({
   isFeatured: z.boolean().optional(),
 });
 
-// Is this needed?
-export const PropertyAddParsedSchema = PropertyAddFormSchema.extend({
-  images: z.array(z.string()).min(1, { message: "At least one image is required" }).max(4, { message: "Maximum of 4 images allowed" }),
-  owner: z.string(),
-});
-
 export const PropertyGetSchema = PropertyAddFormSchema.extend({
   id: z.string().regex(/^[0-9a-f]{24}$/),
   owner: z.string(),
@@ -97,32 +90,8 @@ export const PropertiesGetSchema = z.array(PropertyGetSchema.extend({ images: z.
 
 export type TPropertyAddFormState = z.infer<typeof PropertyAddFormSchema>;
 export type TPropertyEditFormState = TPropertyAddFormState & { id?: string };
-// Is this needed?
-export type TPropertyAddFormParsedState = z.infer<typeof PropertyAddParsedSchema>;
 export type TPropertyRates = z.infer<typeof PropertyRatesSchema>;
 export type TProperty = z.infer<typeof PropertyGetSchema>;
-
-// z.array(
-//   z
-//     .instanceof(File)
-//     .refine((file) => !file || file.size <= MAX_UPLOAD_SIZE, `File must be less than ${MAX_UPLOAD_SIZE}MB`)
-//     .transform((file) => file.name)
-// ),
-
-// z
-//     .array(z.custom<File>())
-//     .refine(
-//       (files) => {
-//         // Check if all items in the array are instances of the File object
-//         return files.every((file) => file instanceof File);
-//       },
-//       {
-//         // If the refinement fails, throw an error with this message
-//         message: "Expected a file",
-//       }
-//     )
-//     .refine((files) => files.every((file) => file.size <= MAX_UPLOAD_SIZE), `File size should be less than ${MAX_UPLOAD_SIZE}mb.`)
-//     .transform((files) => files.map((file) => file.name))
 
 export const PropertySearchSchema = z.object({
   location: z.string().optional(),
