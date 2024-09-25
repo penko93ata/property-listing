52aa822a8eb9607e1c96aaaecd318bd4d8985fa1
Penko Atanasov <atanasovpenko@gmail.com>
2024-09-25 20:45:51 +0300

    Fixing profile image and text overflow

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -24,20 +24,20 @@ export default async function ProfilePage() {
         <div className='bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0'>
           <h1 className='text-3xl font-bold mb-4'>Your Profile</h1>
           <div className='flex flex-col md:flex-row'>
-            <div className='md:w-1/4 mx-20 mt-10'>
+            <div className='md:w-1/4 mx-20 mt-10 break-words'>
               <div className='mb-4'>
                 <Image
-                  className='h-32 w-32 md:h-48 md:w-48 rounded-full mx-auto md:mx-0'
+                  className='sm:h-32 w-32 md:h-auto md:w-48 rounded-full mx-auto md:mx-0'
                   src={sessionUser.user.image ?? profileDefault}
                   width={200}
                   height={200}
                   alt='User'
                 />
               </div>
-              <h2 className='text-2xl mb-4'>
+              <h2 className='text-xl mb-4'>
                 <span className='font-bold block'>Name: </span> {sessionUser.user.name}
               </h2>
-              <h2 className='text-2xl'>
+              <h2 className='text-xl'>
                 <span className='font-bold block'>Email: </span> {sessionUser.user.email}
               </h2>
             </div>
